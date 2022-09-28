0200d6d4bfa0ad9b57d6297860fc5eda322bae90
David Horowitz <njdave71@gmail.com>
2022-09-28 14:05:01 -0400

    adding cart context shopping icon cart dropdown

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { ReactComponent as ShppingIcon } from "../../assests/shopping-bag.svg";
+
+import { CartContext } from "../../contexts/cart.context";
+import "./cart-icon.styles.scss";
+
+const CartIcon = () => {
+  const {isCartOpen, setIsCartOpen } =useContext(CartContext);
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    return (
+        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+            <ShppingIcon className="shopping-icon" />
+            <span className="item-count">0</span>
+        </div>
+    );
+};
+
+export default CartIcon;
